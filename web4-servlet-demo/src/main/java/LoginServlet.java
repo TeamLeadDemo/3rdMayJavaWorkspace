1d954418d327062a003ac9cf280addd8ab4bb5ce
DeveloperOneDemo <92218513+DeveloperOneDemo@users.noreply.github.com>
2022-05-26 12:44:05 -0400

    web4

---
+++ web4-servlet-demo/src/main/java/LoginServlet.java
@@ -0,0 +1,54 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// take out the request parametrs from requuest
+		String userName = request.getParameter("user");
+		String password = request.getParameter("pword");
+		
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		if("admin".equals(userName) && "admin".equals(password)) {
+			//out.println("<h3>Login successfull</h3>");
+			response.sendRedirect("http://localhost:8888/web4-servlet-demo/LoginSuccessFile.html");
+			
+		}else {
+			//out.println("<h3>Login failed!</h3>");
+			response.sendRedirect("http://localhost:8888/web4-servlet-demo/LoginFailedFile.html");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ web4-servlet-demo/src/main/java/MyFirstServlet.java
@@ -0,0 +1,47 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyFirstServlet
+ */
+public class MyFirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MyFirstServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// since this is the first servlet, we are going to write output to the response
+		// first, connect a PrintWriter stream to the response
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		// now you could write your out put to the stream 
+		out.println("<h2>This is my first Servlet!!</h2>");
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
