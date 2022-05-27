b4b380ad520880bd9431442a1dc782820c3bf578
DeveloperOneDemo <92218513+DeveloperOneDemo@users.noreply.github.com>
2022-05-27 13:18:07 -0400

    book-crud-javalin

--- book-management-system-javalin-maven/src/main/java/BookCrud.java
+++ book-management-system-javalin-maven/src/main/java/BookCrud.java
@@ -1,9 +1,18 @@
+import java.util.List;
+
 import io.javalin.Javalin;
+import pojo.BookPojo;
+import service.BookService;
+import service.BookServiceImpl;
 
 
 public class BookCrud {
 
 	public static void main(String[] args) {
+		
+		// create an object of service layer to call its methods in each endpoint method
+		BookService bookService = new BookServiceImpl();
+		
 		Javalin server = Javalin.create(); // javalin creates the jetty server (the default ), we can change the internal server if we want to
 		server.start(7474); // javlin starts the server at port 7474
 
@@ -28,6 +37,12 @@ public class BookCrud {
 			// here we contact service, service contacts dao 
 			// dao fetches all the books and return it back here
 			
+			// allBooks contains all the books fetched from the DB
+			List<BookPojo> allBooks = bookService.getAllBooks();
+			
+			//now put the books in the response body, it has to converted to json format, 
+			// the ctx.json() will take care of the above 2 and sends back the response to the client/consumer
+			ctx.json(allBooks);
 			
 		});
 		
